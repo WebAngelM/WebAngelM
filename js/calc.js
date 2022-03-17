e7b9dd2293b11c1e7c087eb95f453622e75f360e
Angel <mirmsangel@protonmail.com>
2022-03-17 18:31:45 +0300

    add calc function to index

--- js/calc.js
+++ js/calc.js
@@ -1,31 +1,28 @@
-
-
-
-var n1 = document.getElementById('Num1');
-var n2 = document.getElementById('Num2');
-var res = document.getElementById('result');
-
-document.getElementById('add').addEventListener("click", function () {
-    res.value =
-        parseInt(n1.value) + parseInt(n2.value);
-});
-document.getElementById('sub').addEventListener("click", function () {
-    res.value =
-        parseInt(n1.value) - parseInt(n2.value);
-});
-document.getElementById('mul').addEventListener("click", function () {
-    res.value =
-        parseInt(n1.value) * parseInt(n2.value);
-});
-document.getElementById('div').addEventListener("click", function () {
-    res.value =
-        parseInt(n1.value) / parseInt(n2.value);
-});
-document.getElementById('clear_res').addEventListener("click", function () {
-    res.value = ''
-});
-document.getElementById('clear_all').addEventListener("click", function () {
-    res.value = '', n1.value = '', n2.value = ''
-});
-
-
+function calc() {
+    let n1 = document.getElementById('Num1');
+    let n2 = document.getElementById('Num2');
+    let res = document.getElementById('result');
+    
+    document.getElementById('add').addEventListener("click", function () {
+        res.value =
+            parseInt(n1.value) + parseInt(n2.value);
+    });
+    document.getElementById('sub').addEventListener("click", function () {
+        res.value =
+            parseInt(n1.value) - parseInt(n2.value);
+    });
+    document.getElementById('mul').addEventListener("click", function () {
+        res.value =
+            parseInt(n1.value) * parseInt(n2.value);
+    });
+    document.getElementById('div').addEventListener("click", function () {
+        res.value =
+            parseInt(n1.value) / parseInt(n2.value);
+    });
+    document.getElementById('clear_res').addEventListener("click", function () {
+        res.value = '';
+    });
+    document.getElementById('clear_all').addEventListener("click", function () {
+        res.value = '', n1.value = '', n2.value = '';
+    });
+}
